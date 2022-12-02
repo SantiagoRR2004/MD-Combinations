f341885533db007395017621bce3382c8321954e
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2022-12-02 19:12:30 +0100

    Update Traballo_MD_2.5.py

--- Traballo_MD_2.5.py
+++ Traballo_MD_2.5.py
@@ -1,2 +1,10 @@
-gdftgsdfsdf
-drfsdfsdf
+def factorial_iterativa(n):
+    toret = n
+    for i in range (n-1,1,-1):
+        toret *= i
+    return toret
+
+def factorial(n):
+    if n == 1:
+        return(1)    
+    return n * factorial(n - 1)
