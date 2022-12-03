4c42779f172c5d6b83e00b3a4148d575bc2bc7eb
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2022-12-03 16:03:55 +0100

    Update Traballo_MD_2.5.py

--- Traballo_MD_2.5.py
+++ Traballo_MD_2.5.py
@@ -1,3 +1,7 @@
+####################################### Imports #######################################
+
+
+####################################### Functions #######################################
 def factorial_iterativa(n):
     toret = n
     for i in range (n-1,1,-1):
@@ -15,3 +19,25 @@ def combinaciones_sin_repeticiones(n, r):
 
 def combinaciones_con_repeticion(n, r):
     return combinaciones_sin_repeticiones(n+r-1, r)
+
+####################################### Variables #######################################
+conbinatoriaSinR = []
+conbinatoriaConR = []
+
+####################################### Main Program #######################################
+temasTotales = input("¿Cuántos temas entran en el exámen? ").split()[0]
+while not temasTotales.isnumeric() or int(temasTotales) <= 0:
+    print("Debe escribir un número entero mayor que cero")
+    temasTotales = (input("¿Cuántos temas entran en el exámen? ").split())[0]
+temasTotales = int(temasTotales)
+
+    
+n = input("¿Cuántas preguntas tiene el exámen? ").split()[0]
+while not n.isnumeric() or int(n) <= 0:
+    print("Debe escribir un número entero mayor que cero")
+    n = input("¿Cuántas preguntas tiene el exámen? ").split()[0]
+
+temasEstudiados = input("¿Cuántas temas ha estudiado? ").split()[0]
+while not temasEstudiados.isnumeric() or int(temasEstudiados) <= 0:
+    print("Debe escribir un número entero mayor que cero")
+    temasEstudiados = input("¿Cuántas temas ha estudiado? ").split()[0]
