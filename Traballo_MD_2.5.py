1e76c99fe88181bb8387f86e7f9a5a7c39fd3f0d
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2022-12-03 16:43:22 +0100

    Update Traballo_MD_2.5.py
    Ya está casi todo

--- Traballo_MD_2.5.py
+++ Traballo_MD_2.5.py
@@ -9,13 +9,13 @@ def factorial_iterativa(n):
     return toret
 
 def factorial(n):
-    if n == 0:
+    if n <= 0:
         return 1    
     return n * factorial(n - 1)
 
 def combinaciones_sin_repeticiones(n, r):
     combinacion = factorial(n) / (factorial(n-r)*factorial(r))
-    return combinacion
+    return int(combinacion)
 
 def combinaciones_con_repeticion(n, r):
     return combinaciones_sin_repeticiones(n+r-1, r)
@@ -31,13 +31,43 @@ while not temasTotales.isnumeric() or int(temasTotales) <= 0:
     temasTotales = (input("¿Cuántos temas entran en el exámen? ").split())[0]
 temasTotales = int(temasTotales)
 
-    
-n = input("¿Cuántas preguntas tiene el exámen? ").split()[0]
-while not n.isnumeric() or int(n) <= 0:
+numPreguntas = input("¿Cuántas preguntas tiene el exámen? ").split()[0]
+while not numPreguntas.isnumeric() or int(numPreguntas) <= 0:
     print("Debe escribir un número entero mayor que cero")
-    n = input("¿Cuántas preguntas tiene el exámen? ").split()[0]
+    numPreguntas = input("¿Cuántas preguntas tiene el exámen? ").split()[0]
+numPreguntas = int(numPreguntas)    
 
 temasEstudiados = input("¿Cuántas temas ha estudiado? ").split()[0]
 while not temasEstudiados.isnumeric() or int(temasEstudiados) <= 0:
     print("Debe escribir un número entero mayor que cero")
     temasEstudiados = input("¿Cuántas temas ha estudiado? ").split()[0]
+temasEstudiados = int(temasEstudiados)
+
+
+# Calculamos las posibilidades sin repeticiones
+if numPreguntas <= temasTotales:
+    totalSinR = combinaciones_sin_repeticiones(temasTotales, numPreguntas)
+    
+    for i in range(numPreguntas+1):
+        posibilidadesPreguntasEstudiadas = combinaciones_sin_repeticiones(temasEstudiados, i)
+        posibilidadesPreguntasNoEstudiadas = combinaciones_sin_repeticiones(temasTotales-temasEstudiados, numPreguntas-i)
+        conbinatoriaSinR.append(posibilidadesPreguntasEstudiadas*posibilidadesPreguntasNoEstudiadas)
+
+# Calculamos las posibilidades con repeticiones
+totalConR = combinaciones_con_repeticion(temasTotales, numPreguntas)
+for i in range(numPreguntas+1):
+    posibilidadesPreguntasEstudiadas = combinaciones_con_repeticion(temasEstudiados, i)
+    posibilidadesPreguntasNoEstudiadas = combinaciones_con_repeticion(temasTotales-temasEstudiados, numPreguntas-i)
+    conbinatoriaConR.append(posibilidadesPreguntasEstudiadas*posibilidadesPreguntasNoEstudiadas)
+
+if numPreguntas <= temasTotales:
+    for j, i in enumerate(conbinatoriaSinR):
+        print(f"Las posibilidades de acertar {j} sin repetición son {i}")
+else:
+    print("No se puede calcular los porcentajes sin repetir temas porque hay más preguntas que temas.")
+
+
+for j, i in enumerate(conbinatoriaConR):
+        print(f"Las posibilidades de acertar {j} con repetición son {i}")
+
+
