d16a8c3062acda0083da5a03ddddb8fe9be84d67
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2022-12-09 17:09:30 +0100

    Cambio para hacer el algoritmo

--- Traballo_MD_2.5.py
+++ Traballo_MD_2.5.py
@@ -23,9 +23,31 @@ def combinaciones_sin_repeticiones(n, r):
 def combinaciones_con_repeticion(n, r):
     return combinaciones_sin_repeticiones(n+r-1, r)
 
+
+def algoritmo(opciones, huecos, opcionesImportantes):
+    
+    # Calculamos las posibilidades con repeticiones
+    combinatoriaConR = []
+    combinatoriaSinR = []
+    for i in range(huecos+1):
+        posibilidadesPositivas = combinaciones_con_repeticion(opcionesImportantes, i)
+        posibilidadesNegativas = combinaciones_con_repeticion(opciones-opcionesImportantes, huecos-i)
+        combinatoriaConR.append(posibilidadesPositivas*posibilidadesNegativas)
+    
+    if opciones < huecos:
+        return combinatoriaConR
+    
+    # Calculamos las posibilidades sin repeticiones
+    else:
+        combinatoriaSinR = []
+        for i in range(huecos+1):
+            posibilidadesPositivas = combinaciones_sin_repeticiones(opcionesImportantes, i)
+            posibilidadesNegativas = combinaciones_sin_repeticiones(opciones-opcionesImportantes, huecos-i)
+            combinatoriaSinR.append(posibilidadesPositivas*posibilidadesNegativas)
+        return combinatoriaSinR, combinatoriaConR
+
+
 ####################################### Variables #######################################
-conbinatoriaSinR = []
-conbinatoriaConR = []
 
 ####################################### Main Program #######################################
 temasTotales = input("¿Cuántos temas entran en el exámen? ").split()[0]
@@ -46,39 +68,31 @@ while not temasEstudiados.isnumeric() or int(temasEstudiados) <= 0:
     temasEstudiados = input("¿Cuántas temas ha estudiado? ").split()[0]
 temasEstudiados = int(temasEstudiados)
 
+if numPreguntas >= temasTotales:
+    combinatoriaConR = algoritmo(temasTotales,numPreguntas,temasEstudiados)
+else:
+    combinatoriaSinR, combinatoriaConR = algoritmo(temasTotales,numPreguntas,temasEstudiados)
+    totalSinR = sum(combinatoriaSinR)
 
-# Calculamos las posibilidades sin repeticiones
-if numPreguntas <= temasTotales:
-    totalSinR = combinaciones_sin_repeticiones(temasTotales, numPreguntas)
-    
-    for i in range(numPreguntas+1):
-        posibilidadesPreguntasEstudiadas = combinaciones_sin_repeticiones(temasEstudiados, i)
-        posibilidadesPreguntasNoEstudiadas = combinaciones_sin_repeticiones(temasTotales-temasEstudiados, numPreguntas-i)
-        conbinatoriaSinR.append(posibilidadesPreguntasEstudiadas*posibilidadesPreguntasNoEstudiadas)
+totalConR = sum(combinatoriaConR)
 
-# Calculamos las posibilidades con repeticiones
-totalConR = combinaciones_con_repeticion(temasTotales, numPreguntas)
-for i in range(numPreguntas+1):
-    posibilidadesPreguntasEstudiadas = combinaciones_con_repeticion(temasEstudiados, i)
-    posibilidadesPreguntasNoEstudiadas = combinaciones_con_repeticion(temasTotales-temasEstudiados, numPreguntas-i)
-    conbinatoriaConR.append(posibilidadesPreguntasEstudiadas*posibilidadesPreguntasNoEstudiadas)
 
 if numPreguntas > temasTotales:
     print("No se puede calcular los porcentajes sin repetir temas porque hay más preguntas que temas.")
 else:
     print("\nA continuación mostramos las probabilidades sin repetir temas en distintas preguntas del examen\n")
-    for j, i in enumerate(conbinatoriaSinR):
+    for j, i in enumerate(combinatoriaSinR):
         print(f"La probabilidad de acertar {j} preguntas es de {i/totalSinR*100:.2f}%")
     print("")
-    for i in range(len(conbinatoriaSinR)-1):
-        print(f"La probabilidad de acertar más de {i} preguntas es de {sum(conbinatoriaSinR[i+1:])/totalSinR*100:.2f}%")
+    for i in range(len(combinatoriaSinR)-1):
+        print(f"La probabilidad de acertar más de {i} preguntas es de {sum(combinatoriaSinR[i+1:])/totalSinR*100:.2f}%")
 
 print("\nA continuación mostramos las probabilidades repitiendo temas en distintas preguntas del examen\n")
-for j, i in enumerate(conbinatoriaConR):
+for j, i in enumerate(combinatoriaConR):
     print(f"La probabilidad de acertar {j} preguntas es de {i/totalConR*100:.2f}%")
 print("")
-for i in range(len(conbinatoriaConR)-1):
-    print(f"La probabilidad de acertar más de {i} preguntas es de {sum(conbinatoriaConR[i+1:])/totalConR*100:.2f}%")
+for i in range(len(combinatoriaConR)-1):
+    print(f"La probabilidad de acertar más de {i} preguntas es de {sum(combinatoriaConR[i+1:])/totalConR*100:.2f}%")
     
 plt.figure(figsize=(17,9))
 plt.title(f"{temasTotales} temas, {numPreguntas} preguntas, {temasEstudiados} estudiados", fontsize=22)
@@ -90,24 +104,24 @@ plt.xticks([x for x in range(numPreguntas+1)])
 # Los prints comentados están para comprobar que funcione
 if numPreguntas <= temasTotales:
 ##    print([x for x in range(numPreguntas+1)])
-##    print([x/totalSinR*100 for x in conbinatoriaSinR])
-    plt.scatter([x for x in range(numPreguntas+1)], [x/totalSinR*100 for x in conbinatoriaSinR], s=50, c="r", zorder=10, marker="o")
-    plt.plot([x for x in range(numPreguntas+1)], [x/totalSinR*100 for x in conbinatoriaSinR], linewidth=5, c="r", zorder=0, label="Sin repetición\nacertar\nx preguntas")
+##    print([x/totalSinR*100 for x in combinatoriaSinR])
+    plt.scatter([x for x in range(numPreguntas+1)], [x/totalSinR*100 for x in combinatoriaSinR], s=50, c="r", zorder=10, marker="o")
+    plt.plot([x for x in range(numPreguntas+1)], [x/totalSinR*100 for x in combinatoriaSinR], linewidth=5, c="r", zorder=0, label="Sin repetición\nacertar\nx preguntas")
 
 ##    print([x for x in range(numPreguntas)])
-##    print([sum(conbinatoriaSinR[x+1:])/totalSinR*100 for x in range(len(conbinatoriaSinR)-1)])
-    plt.scatter([x for x in range(numPreguntas)], [sum(conbinatoriaSinR[x+1:])/totalSinR*100 for x in range(len(conbinatoriaSinR)-1)], s=50, c="#FF00FF", zorder=10, marker="o")
-    plt.plot([x for x in range(numPreguntas)], [sum(conbinatoriaSinR[x+1:])/totalSinR*100 for x in range(len(conbinatoriaSinR)-1)], linewidth=5, c="#FF00FF", zorder=0, label="Sin repetición\nacertar\nmás de x\npreguntas")
+##    print([sum(combinatoriaSinR[x+1:])/totalSinR*100 for x in range(len(combinatoriaSinR)-1)])
+    plt.scatter([x for x in range(numPreguntas)], [sum(combinatoriaSinR[x+1:])/totalSinR*100 for x in range(len(combinatoriaSinR)-1)], s=50, c="#FF00FF", zorder=10, marker="o")
+    plt.plot([x for x in range(numPreguntas)], [sum(combinatoriaSinR[x+1:])/totalSinR*100 for x in range(len(combinatoriaSinR)-1)], linewidth=5, c="#FF00FF", zorder=0, label="Sin repetición\nacertar\nmás de x\npreguntas")
 
 ##print([x for x in range(numPreguntas+1)])
-##print([x/totalConR*100 for x in conbinatoriaConR])
-plt.scatter([x for x in range(numPreguntas+1)], [x/totalConR*100 for x in conbinatoriaConR], s=50, c="b", zorder=10, marker="o")
-plt.plot([x for x in range(numPreguntas+1)], [x/totalConR*100 for x in conbinatoriaConR], linewidth=5, c="b", zorder=0, label="Con repetición\nacertar\nx preguntas")
+##print([x/totalConR*100 for x in combinatoriaConR])
+plt.scatter([x for x in range(numPreguntas+1)], [x/totalConR*100 for x in combinatoriaConR], s=50, c="b", zorder=10, marker="o")
+plt.plot([x for x in range(numPreguntas+1)], [x/totalConR*100 for x in combinatoriaConR], linewidth=5, c="b", zorder=0, label="Con repetición\nacertar\nx preguntas")
 
 ##print([x for x in range(numPreguntas)])
-##print([sum(conbinatoriaConR[x+1:])/totalConR*100 for x in range(len(conbinatoriaConR)-1)])
-plt.scatter([x for x in range(numPreguntas)], [sum(conbinatoriaConR[x+1:])/totalConR*100 for x in range(len(conbinatoriaConR)-1)], s=50, c="#00FCFF", zorder=10, marker="o")
-plt.plot([x for x in range(numPreguntas)], [sum(conbinatoriaConR[x+1:])/totalConR*100 for x in range(len(conbinatoriaConR)-1)], linewidth=5, c="#00FCFF", zorder=0, label="Con repetición\nacertar\nmás de x\npreguntas")
+##print([sum(combinatoriaConR[x+1:])/totalConR*100 for x in range(len(combinatoriaConR)-1)])
+plt.scatter([x for x in range(numPreguntas)], [sum(combinatoriaConR[x+1:])/totalConR*100 for x in range(len(combinatoriaConR)-1)], s=50, c="#00FCFF", zorder=10, marker="o")
+plt.plot([x for x in range(numPreguntas)], [sum(combinatoriaConR[x+1:])/totalConR*100 for x in range(len(combinatoriaConR)-1)], linewidth=5, c="#00FCFF", zorder=0, label="Con repetición\nacertar\nmás de x\npreguntas")
 
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), shadow=True, fancybox=True)
 plt.xlabel("Preguntas")
